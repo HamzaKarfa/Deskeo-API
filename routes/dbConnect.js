aa84808283e9eb23c09badce2af9de782a3f40aa
HamzaKarfa <hamza.karfa@gmail.com>
2020-10-05 11:12:45 +0200

    commit 05/10/2020 fonctionnel

--- routes/dbConnect.js
+++ routes/dbConnect.js
@@ -9,7 +9,7 @@ function dbConnect() {
     return (
         mysql.createConnection({
             database: 'living_app',
-            host: "localhost",
+            host: "http://pma.onevalue.fr/db_structure.php?server=1&db=living_app",
             user: "living_app",
             password: "S6M7FCcWJeQjhNW"
         })
